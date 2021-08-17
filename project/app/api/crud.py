86f0b01d07ecf3feb5c9bd9283d2a57cf20a8901
qu3vipon <sdh5813@gmail.com>
2021-08-17 11:41:14 +0900

    first commit: fastapi-tdd-docker

---
+++ project/app/api/crud.py
@@ -0,0 +1,25 @@
+from typing import Union, List
+
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
+
+
+async def post(payload: SummaryPayloadSchema) -> list:
+    summary = TextSummary(
+        url=payload.url,
+        summary="dummy summary",
+    )
+    await summary.save()
+    return summary.id
